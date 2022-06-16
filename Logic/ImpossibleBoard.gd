ac7b6fda513e511745b5595ce340b027bc4d4369
nwallenfang <nils@wallenfang.de>
2022-06-16 17:05:04 +0200

    build some stuff into level

---
+++ Logic/ImpossibleBoard.gd
@@ -0,0 +1,64 @@
+extends Spatial
+
+export var dim_x = 3
+export var dim_y = 3
+
+# tile references
+onready var tiles = [[$Tile11, $Tile12, $Tile13], [$Tile21, $Tile22, $Tile23], [$Tile31, $Tile32, $Tile33]]
+
+
+func _ready() -> void:
+	for tile_row in tiles:
+		for tile in tile_row:
+			tile.connect("flipped", self, "register_tile_change")
+
+func check_victory() -> bool:
+	for tile_row in tiles:
+		for tile in tile_row:
+			if not tile.is_flipped:
+				return false
+	return true
+
+
+func diagonal_condition(x_flipped, y_flipped, x_other, y_other):
+	# possible to solve in 3x3 but pretty difficult, easy with a weight
+	if y_flipped-1 == y_other and x_flipped + 1 == x_other:
+		return true
+	if y_flipped-1 == y_other and x_flipped - 1 == x_other:
+		return true
+	if y_flipped+1 == y_other and x_flipped + 1 == x_other:
+		return true
+	if y_flipped+1 == y_other and x_flipped - 1 == x_other:
+		return true
+	return false
+	
+func cardinal_condition(x_flipped, y_flipped, x_other, y_other):
+	if y_flipped-1 == y_other and x_flipped == x_other:
+		return true
+	if y_flipped+1 == y_other and x_flipped == x_other:
+		return true
+	if y_flipped == y_other and x_flipped + 1 == x_other:
+		return true
+	if y_flipped == y_other and x_flipped - 1 == x_other:
+		return true
+
+func check_flip_condition(x_flipped, y_flipped, x_other, y_other) -> bool:
+	# flip everything
+	if x_flipped != x_other or y_flipped != y_other:
+		return true
+	else:
+		return false
+
+func register_tile_change(x, y):
+	# changing rule, which tiles will flip as well
+	for tile_row in tiles:
+		for tile in tile_row:
+			if check_flip_condition(x, y, tile.x, tile.y):
+				# if there's a weight on here don't flip
+				if Game.weight_x == tile.x and Game.weight_y == tile.y:
+					pass # don't flip
+				else:
+					tile.flip()
+	
+	if check_victory():
+		print("wow you won")

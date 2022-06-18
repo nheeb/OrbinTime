a4987d80c3a407cffb223d46bd00c217b4020c57
nheeb <nitai64@yahoo.de>
2022-06-18 20:03:09 +0200

    LED Light

--- Objects/LED.gd
+++ Objects/LED.gd
@@ -7,3 +7,4 @@ func _ready():
 
 func set_light(led : String, value: bool) -> void:
 	get_node(led).get_active_material(0).emission_enabled = value
+	get_node(led).get_node("OmniLight").light_energy = 1.0 if value else 0.0

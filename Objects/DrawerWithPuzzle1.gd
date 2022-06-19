d3ac437c4483fd29d44365849b9656cd979e8fb8
nheeb <nitai64@yahoo.de>
2022-06-19 17:08:33 +0200

    Lamps

--- Objects/DrawerWithPuzzle1.gd
+++ Objects/DrawerWithPuzzle1.gd
@@ -2,9 +2,14 @@ extends Spatial
 
 export var target_color: Color
 
+func _ready():
+	if has_node("Drawer/Lamp/Light"):
+		$Drawer/Lamp/Light.light_energy = 0.0
 
 func _on_BoardPuzzle_puzzle_won() -> void:
 	Game.turn_orb_on($Drawer/SolutionOrb.get_active_material(0), target_color)
+	if has_node("Drawer/Lamp/Light"):
+		$Drawer/Lamp/Light.light_energy = 1.0
 	if has_node("Sound"):
 		if not $Sound.playing:
 			$Sound.play()

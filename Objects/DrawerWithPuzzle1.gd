646c2c55d1ed9efb35c4761f596c4b96e489dfbe
nheeb <nitai64@yahoo.de>
2022-06-19 17:36:44 +0200

    Lamps

--- Objects/DrawerWithPuzzle1.gd
+++ Objects/DrawerWithPuzzle1.gd
@@ -4,12 +4,12 @@ export var target_color: Color
 
 func _ready():
 	if has_node("Drawer/Lamp/Light"):
-		$Drawer/Lamp/Light.light_energy = 0.0
+		$Drawer/Lamp/Light.visible = false
 
 func _on_BoardPuzzle_puzzle_won() -> void:
 	Game.turn_orb_on($Drawer/SolutionOrb.get_active_material(0), target_color)
 	if has_node("Drawer/Lamp/Light"):
-		$Drawer/Lamp/Light.light_energy = 1.0
+		$Drawer/Lamp/Light.visible = true
 	if has_node("Sound"):
 		if not $Sound.playing:
 			$Sound.play()
